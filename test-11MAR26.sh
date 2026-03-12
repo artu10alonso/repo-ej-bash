71f6b3734519ba23a0ec43530b3c84a1e9682213
Arturo <arturo@test2.com>
2026-03-12 20:07:22 +0100

    branch_V1_2_12MAR26

--- test-11MAR26.sh
+++ test-11MAR26.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
+export PATH="$PATH:/bin:/usr/bin"
 
 START_T=$(date +%s)
 
-read -p "Introduce ruta para backup " BU_PATH
+read -p "Introduce ruta para backup " BU_FOLDER
+MY_PATH="$(pwd)/$BU_FOLDER"
+echo "$PATH"
 
-if [[ ! -d BU_PATH]]; then
+if [[ ! -d $MY_PATH ]]; then
     echo "La ruta no existe. Fin de script"
     exit 1
 fi 
 
-mkdir backups
+mkdir -p backups
 
-DATE="$(date)
+DATE="$(date)"
 
-tar -czvf "$DATE"_"$BU_PATH"_backup.tar.gz backups
-TAR_SIZE=$(stat --format =%s $DATE"_"$BU_PATH"_backup.tar.gz)
-mv $DATE"_"$BU_PATH"_backup.tar.gz backups
+tar -czvf "$DATE"_"$BU_FOLDER"_backup.tar.gz backups
+TAR_SIZE=$(stat --format =%s $DATE"_"$BU_FOLDER"_backup.tar.gz)
+mv $DATE"_"$BU_FOLDER"_backup.tar.gz backups
 
-echo "Se ha comprimido '$BU_PATH" en el archivo '$DATE'_'$BU_PATH_backup.tar.gz"
+echo "Se ha comprimido '$BU_PATH" en el archivo '$DATE'_'$BU_FOLDER_backup.tar.gz"
 echo "Tiene un peso de '$TAR_SIZE'"
 
 
