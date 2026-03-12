8df1292b2574b6b3bced893d75118bbce4d8088b
Arturo <arturo@test2.com>
2026-03-12 19:37:03 +0100

    Version V1_12MAR26

--- test-11MAR26.sh
+++ test-11MAR26.sh
@@ -2,5 +2,27 @@
 
 START_T=$(date +%s)
 
+read -p "Introduce ruta para backup " BU_PATH
+
+if [[ ! -d BU_PATH]]; then
+    echo "La ruta no existe. Fin de script"
+    exit 1
+fi 
+
+mkdir backups
+
+DATE="$(date)
+
+tar -czvf "$DATE"_"$BU_PATH"_backup.tar.gz backups
+
+TAR_SIZE=$(stat --format =%s $DATE"_"$BU_PATH"_backup.tar.gz)
+
+echo "Se ha comprimido '$BU_PATH" en el archivo '$DATE'_'$BU_PATH_backup.tar.gz"
+echo "Tiene un peso de '$TAR_SIZE'"
+
+
+
+
+
 
 END_T=$(date +%s)
